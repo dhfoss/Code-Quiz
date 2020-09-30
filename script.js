608904e0bf0fa29c159ec1bef07f82f105703716
Daniel Hawthorne-Foss <dhfoss89@gmail.com>
2020-09-30 14:44:18 -0400

    timer stops if user finishes the quis in time.

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ var currentQuestion = 1;
 var rightAnswers = 0;
 var numberOfQuestions = 10;
 var timer = document.querySelector("#timer");
-var timeLeft = 5;
+var timeLeft = 10;
 
 
 
@@ -17,12 +17,17 @@ var timeLeft = 5;
 //===========
 
 
-// Timer 
+// Timer function.  This will cound down how much time the user has left to finish the quiz.
+// If the user finishes the quiz before the timer runs out, the timer will stop.
 function setTimer() {
+    timer.textContent = timeLeft;
     var timerInterval = setInterval(function() {
-        timer.textContent = timeLeft
         timeLeft--;
-        if (timeLeft === 0) {
+        timer.textContent = timeLeft;
+        if (currentQuestion > numberOfQuestions) {
+            clearInterval(timerInterval);
+        }
+        if (timeLeft < 0) {
             clearInterval(timerInterval);
             main.classList.add("hidden");
             alert("End of quiz. You got " + rightAnswers + " right, and " + (numberOfQuestions - rightAnswers) + " wrong.");
@@ -34,14 +39,8 @@ function setTimer() {
 setTimer();
 
 
-
-
-
-
-
-
 // Functio to continue the quiz. It will advance the user to the next question when they answer a question.
-// When the last question is answered, it notifies the user of how many questions they anwered right and wrong.
+// When the last question is answered it notifies the user of how many questions they anwered right and wrong.
     //  TO DO ---> It then asks if the user wants to save their score on the app.
 function continueQuiz() {
     var answeredQuestion = document.querySelector("#question" + currentQuestion);
@@ -61,6 +60,7 @@ function continueQuiz() {
 // EVENT LISTENERS
 //=================
 
+//This detects if the user clicks a button. If they answered right, their number of right answers is updated.
 main.addEventListener("click", function(event) {
     var button = event.target;
     var rightAnswer = button.classList;
