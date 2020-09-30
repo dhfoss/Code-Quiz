f26b5233057a1e7853b59d80e254dd876bd683cb
Daniel Hawthorne-Foss <dhfoss89@gmail.com>
2020-09-30 11:53:29 -0400

    Questions now advance when a button is clicked

--- script.js
+++ script.js
@@ -1,4 +1,38 @@
-var buttons = document.querySelectorAll("button");
+//===========
+// VARIABLES
+//===========
+
+var main = document.querySelector("main");
+var currentQuestion = 1;
 
 
-buttons.innerHTML = "test";
+
+
+
+//===========
+// FUNCTIONS
+//===========
+
+function continueQuiz() {
+    var answeredQuestion = document.querySelector("#question" + currentQuestion);
+    answeredQuestion.setAttribute("class", "hidden");
+    currentQuestion++;
+    var nextQuestion = document.querySelector("#question" + currentQuestion);
+    nextQuestion.setAttribute("class", "");
+}
+
+
+
+
+
+
+//=================
+// EVENT LISTENERS
+//=================
+
+main.addEventListener("click", function(event) {
+    var button = event.target;
+    if (button.matches("button")) {
+        continueQuiz();
+    }
+})
