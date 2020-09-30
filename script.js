9aefdaa0d75f45253083185d8ca9b65e7149227b
Daniel Hawthorne-Foss <dhfoss89@gmail.com>
2020-09-30 14:17:23 -0400

    added a timer. 5 seconds for now

--- script.js
+++ script.js
@@ -4,7 +4,10 @@
 
 var main = document.querySelector("main");
 var currentQuestion = 1;
-
+var rightAnswers = 0;
+var numberOfQuestions = 10;
+var timer = document.querySelector("#timer");
+var timeLeft = 5;
 
 
 
@@ -13,16 +16,44 @@ var currentQuestion = 1;
 // FUNCTIONS
 //===========
 
-function continueQuiz() {
-    var answeredQuestion = document.querySelector("#question" + currentQuestion);
-    answeredQuestion.setAttribute("class", "hidden");
-    currentQuestion++;
-    var nextQuestion = document.querySelector("#question" + currentQuestion);
-    nextQuestion.setAttribute("class", "");
+
+// Timer 
+function setTimer() {
+    var timerInterval = setInterval(function() {
+        timer.textContent = timeLeft
+        timeLeft--;
+        if (timeLeft === 0) {
+            clearInterval(timerInterval);
+            main.classList.add("hidden");
+            alert("End of quiz. You got " + rightAnswers + " right, and " + (numberOfQuestions - rightAnswers) + " wrong.");
+        }
+    }, 1000);
 }
 
 
+setTimer();
 
+
+
+
+
+
+
+
+// Functio to continue the quiz. It will advance the user to the next question when they answer a question.
+// When the last question is answered, it notifies the user of how many questions they anwered right and wrong.
+    //  TO DO ---> It then asks if the user wants to save their score on the app.
+function continueQuiz() {
+    var answeredQuestion = document.querySelector("#question" + currentQuestion);
+    answeredQuestion.classList.add("hidden");
+    currentQuestion++;
+    if (currentQuestion <= numberOfQuestions) {
+        var nextQuestion = document.querySelector("#question" + currentQuestion);
+        nextQuestion.classList.remove("hidden");    
+    } else {
+        alert("End of quiz. You got " + rightAnswers + " right, and " + (numberOfQuestions - rightAnswers) + " wrong.");
+    }
+}
 
 
 
@@ -32,6 +63,11 @@ function continueQuiz() {
 
 main.addEventListener("click", function(event) {
     var button = event.target;
+    var rightAnswer = button.classList;
+    console.log(rightAnswer);
+    if (rightAnswer == "rightAnswer") {
+        rightAnswers++;
+    }
     if (button.matches("button")) {
         continueQuiz();
     }
